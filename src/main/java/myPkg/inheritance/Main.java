52b37ac7681eb633790ec9ff30dec03e1979b6af
chacloutier <70588238+chacloutier@users.noreply.github.com>
2020-09-01 10:44:05 -0400

    Update Main.java

--- src/main/java/myPkg/inheritance/Main.java
+++ src/main/java/myPkg/inheritance/Main.java
@@ -14,9 +14,9 @@ public class Main {
     public static void main(String[] args) {  
     Scanner input = new Scanner(System.in);
     
-    //MyStack stack = new MyStack();
+    MyStack stack = new MyStack();
     
-    /*for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
       stack.push(input.nextLine());
     
     System.out.println(stack.getSize() + " strings are: ");
